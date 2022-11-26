d8b67b7076fae57c646f315c90a2ff74ed3c20b5
Ssemgame <81359690+Ssemgame@users.noreply.github.com>
2022-11-26 17:04:33 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SsemgameAPI
+{
+    public class Class1
+    {
+        public void send(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public void write(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public void convertInt(string cislo, int output)
+        {
+            if (int.TryParse(cislo, out output))
+            {
+            } else
+            {
+                Console.WriteLine("Something went wrong");
+            }
+        }
+        public void getPaste(string output)
+        {
+            output = Console.ReadLine();
+        }
+    }
+}
